035bfba7d9a5d6e2589a6cb65bf1c219eec55129
Max <max.work.dev@gmail.com>
2021-06-14 08:19:09 +0300

    feat: add card styles

--- src/components/molecules/RecipeCard/RecipeCard.style.js
+++ src/components/molecules/RecipeCard/RecipeCard.style.js
@@ -1,6 +1,19 @@
 import styled from "styled-components";
+import Card from "@material-ui/core/Card";
 
-export const Wrapper = styled.div`
-  display: flex;
-  height: 50px;
+export const CardWrapper = styled(Card)`
+  flex-grow: 1;
+`;
+
+export const ImageStyled = styled.img`
+  margin: auto;
+  display: block;
+  max-width: 100%;
+  max-height: 100%;
+
+  transition: 1s;
+
+  :hover {
+    transform: scale(1.1);
+  }
 `;
--- src/components/organisms/RecipeList/RecipeList.js
+++ src/components/organisms/RecipeList/RecipeList.js
@@ -4,25 +4,6 @@ import { RecipeCard } from "../../molecules/RecipeCard/RecipeCard";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 //Style
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  container: {
-    display: "grid",
-    gridTemplateColumns: "repeat(12, 1fr)",
-    gridGap: theme.spacing(3),
-  },
-  paper: {
-    padding: "10px",
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    whiteSpace: "nowrap",
-    marginBottom: "20px",
-  },
-  divider: {
-    margin: theme.spacing(2, 0),
-  },
-}));
 
 const recipes = [
   {
@@ -56,14 +37,12 @@ const recipes = [
 ];
 
 export const RecipeList = () => {
-  const classes = useStyles();
-
   return (
     <Box padding={4}>
       <Grid container spacing={4}>
         {recipes.map((recipe) => (
-          <Grid item xs={12} sm={4}>
-            <RecipeCard img={recipe.img} name={recipe.name} />
+          <Grid item xs={12}>
+            <RecipeCard img={recipe.img} title={recipe.name} />
           </Grid>
         ))}
       </Grid>
